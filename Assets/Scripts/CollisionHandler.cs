5bab4607cb5590939d1bca2768ba073ed8b6e178
Maryam Mozaffari <108430847+MaryamMozaffari99@users.noreply.github.com>
2023-05-11 13:28:17 +0330

    Add files via upload

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,9 @@ using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+
+    [SerializeField] float levelLoadDelay = 2f;
+
     void OnCollisionEnter(Collision other)
     {
        switch (other.gameObject.tag )
@@ -12,44 +16,98 @@ public class CollisionHandler : MonoBehaviour
             case "Friendly":
                 Debug.Log("This thing is friendly");
                 break;
-            case "Fuel":
+            /*case "Fuel":
                 Debug.Log("You picked up the Fuel!");
-                break;
-            case "Finish":
+                break;*/
+                ////case "Finish":
                 //Debug.Log("Congrats, you blew up!");
-                LoadNextLevel();
+                //////    LoadNextLevel();
+            case "Finish":
+                //LoadNextLevel();
+                SrartSuccessSequence();
                 break;
+
+          /*  case "Finishh":
+                Debug.Log("Hora!");
+                break;*/
+            
             default:
                 //Debug.Log("SORRY, you blew up!");
-                ReloadLevel();
+                //ReloadLevel();
+                //RestartCurrentScene();
+                StartCrashSequence();
+                Invoke("ReloadLevel" , 1f);
                 break;
-                
-            }
+
         }
-        void LoadNextLevel()
+
+    }
+
+    private void SrartSuccessSequence()
+    {
+          // throw new NotImplementedException();
+        GetComponent<Movement>().enabled = false;
+        Invoke("LoadNextLevel", levelLoadDelay);
+
+
+    }
+
+    /* void SrartSuccessSequence()
+{
+    // todo add SFX upon crash
+    // todo add particle effect upon crash
+    // Invoke("ReloadLevel", levelLoadDelay);
+    ReloadLevel();
+}*/
+
+
+    void StartCrashSequence()
         {
+        // todo add SFX upon crash
+        // todo add particle effect upon crash
+
+        GetComponent<Movement>().enabled = false;
+        Invoke("ReloadLevel", levelLoadDelay);
+
+        //GetComponent<Movement>().enabled = false;
+        //Invoke("StartCrashSequence", levelLoadDelay);
+
+    }
+    void LoadNextLevel()
+    {
             int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
             int nextSceneIndex = currentSceneIndex + 1;
             if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
             {
                 nextSceneIndex = 0; 
             }
-            
-            //SceneManager.LoadScene(currentSceneIndex);
-            SceneManager.LoadScene(nextSceneIndex);
-
-
-    }
-    void ReloadLevel()
+        else
         {
-            //1 SceneManager.LoadScene(0);
+            //RestartCurrentScene();
+            ReloadLevel();
 
-            //2 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-            
-            //Clean Code
-
-            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-            SceneManager.LoadScene(currentSceneIndex);
         }
-    
+
+        //SceneManager.LoadScene(currentSceneIndex);
+        SceneManager.LoadScene(nextSceneIndex);
+         
+
+        }
+        void ReloadLevel()
+        {
+                //1 SceneManager.LoadScene(0);
+
+                //2 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            
+                //Clean Code
+
+                int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+                SceneManager.LoadScene(currentSceneIndex);
+        }
+   /* void RestartCurrentScene()
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentScene);
+    }*/
+
 }
